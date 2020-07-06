8c8d68ba845bbd4ca94891f40fae04f9ca2d5563
Ramil Saifullin <ramil99.rs@gmail.com>
2020-07-06 12:17:47 +0600

    //

--- Money/src/main/java/ru/omsk/neoLab/race/ARace.java
+++ Money/src/main/java/ru/omsk/neoLab/race/ARace.java
@@ -1,4 +1,21 @@
 package ru.omsk.neoLab.race;
 
-public class ARace {
+public abstract class ARace {
+    private String nameRace;
+    private int quantity; // Число юнитов определенной рассы
+
+    public ARace(String nameRace, int quantity) {
+        this.nameRace = nameRace;
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getNameRace() {
+        return nameRace;
+    }
+
+    public abstract void receiveAdvantage();
 }
---
+++ Money/src/main/java/ru/omsk/neoLab/race/Undead.java
@@ -0,0 +1,12 @@
+package ru.omsk.neoLab.race;
+
+public class Undead extends ARace {
+
+    public Undead(String nameRace, int quantity) {
+        super(nameRace, quantity);
+    }
+
+    public void receiveAdvantage() {
+
+    }
+}
