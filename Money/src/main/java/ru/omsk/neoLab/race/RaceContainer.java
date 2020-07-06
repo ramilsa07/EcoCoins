4e694dd744f5acd0f50bc4abe6300cb792b3fc9e
Ramil Saifullin <ramil99.rs@gmail.com>
2020-07-06 16:26:59 +0600

    [+] RaceContainer

---
+++ Money/src/main/java/ru/omsk/neoLab/race/RaceContainer.java
@@ -0,0 +1,19 @@
+package ru.omsk.neoLab.race;
+
+import java.util.HashSet;
+
+public class RaceContainer {
+    private HashSet<ARace> setRace = new HashSet<>();
+
+    public void addRace(ARace race){
+        setRace.add(race);
+    }
+
+    public int getContainerSize(){
+        return setRace.size();
+    }
+
+    public void removeElement(ARace race){
+        setRace.remove(race);
+    }
+}
