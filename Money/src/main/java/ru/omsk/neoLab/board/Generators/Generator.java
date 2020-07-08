f19c19ef93de9470df9e36c05e5a0973e9b2cb78
Евгений Комар <overlord003@mail.ru>
2020-07-08 14:21:59 +0600

    Генерация

--- Money/src/main/java/ru/omsk/neoLab/board/Generators/Generator.java
+++ Money/src/main/java/ru/omsk/neoLab/board/Generators/Generator.java
@@ -1,15 +1,12 @@
 package ru.omsk.neoLab.board.Generators;
 
 
-
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
 import ru.omsk.neoLab.board.Generators.Calls.Call.*;
-import ru.omsk.neoLab.board.Generators.Calls.ListCall;
 
-import javax.xml.validation.Validator;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Generator implements IGenerator {
@@ -19,21 +16,28 @@ public class Generator implements IGenerator {
     private static ACall[][] board;
     private static int countWater;
 
-    private final static ListCall listCall = new ListCall(new ACall[]{new Earth(),new Mushrooms(),new Mounted(),new Water()});
+    private final static List<ACall> listCall = new ArrayList<ACall>();
     private final Random random = new Random();
 
+    static {
+        listCall.add(new Earth());
+        listCall.add(new Mushrooms());
+        listCall.add(new Mounted());
+        listCall.add(new Water());
+    }
+
     @Override
-    public ACall[][] generate(int height,int width) {
+    public ACall[][] generate(int height, int width) {
         long start = System.currentTimeMillis();
         log.info("Генерация началась");
         board = new ACall[height][width];
         //private ACall[][] board;
 
         countWater = 0;
-        int maxWater = Math.round((float) (height * width) /3);
+        int maxWater = Math.round((float) (height * width) / 3);
 
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < width; j++){
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 board[i][j] = toRandom(maxWater);
                 countWater++;
             }
@@ -59,7 +63,7 @@ public class Generator implements IGenerator {
 
     private ACall toRandom(int maxWater){
 
-        ACall call= listCall.getCall(random.nextInt(4));
+        ACall call = listCall.get(random.nextInt(4));
         if((call.getType().equals("Water")) && (countWater <= maxWater)){
             countWater++;
             return call;
@@ -73,7 +77,7 @@ public class Generator implements IGenerator {
         else {
             //log.info("Попалась снова вода");
         }
-        return listCall.getCall(random.nextInt(3));
+        return listCall.get(random.nextInt(3));
     }
 
     private static void toFind(ACall call){
